450262397ed5caaeaa39c79382dfa644bd3805ea
Saulgsj-dev <saulgsjdev@gmail.com>
2026-04-08 18:54:38 -0300

    voltei para o funcional

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,7 @@
 // src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// supabaseClient.js
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
+const supabaseUrl = 'https://jwmoftfgoiwbfwitcyrf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bW9mdGZnb2l3YmZ3aXRjeXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5Mjg2MjUsImV4cCI6MjA5MDUwNDYyNX0.JRUI4sKa-_Q9JqFnRZxpM9k5ywXJp9DeKlcye1F5SGE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
